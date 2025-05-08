c38eeaaf8e3a0228c882891194dc6cd82afbdd9f
Bohdan  Danylenko <bohdan.danilenko@gmail.com>
2025-05-08 14:48:56 +0200

    added update

--- homework-1/my-react-app/src/main.jsx
+++ homework-1/my-react-app/src/main.jsx
@@ -3,16 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from "react-redux";
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import { store } from './store/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
 
-    <BrowserRouter>
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
 
     </Provider>
   </StrictMode>,
